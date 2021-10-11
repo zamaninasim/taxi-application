950d968106d7cdcfec3d548342927149fc7e66b2
nasim <zamaninasim213@gamail.com>
2021-10-11 16:50:39 +0330

    add location class

---
+++ src/ir/maktab/trip/Location.java
@@ -0,0 +1,27 @@
+package ir.maktab.trip;
+
+public class Location {
+    private String latitude;
+    private String Longitude;
+
+    public Location(String latitude, String longitude) {
+        this.latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        Longitude = longitude;
+    }
+}
